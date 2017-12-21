2260505154082504e639c1b53f8cda0e127826b4
Yilin Yang <yiliny@umich.edu>
2017-12-20 19:26:42 -0500

    Make TerminalOutputTestCase more versatile
    Refactor helper.TerminalOutputTestCase so that its functions can be
    called multiple times from within a single test method.

--- tests/helpers.py
+++ tests/helpers.py
@@ -27,23 +27,56 @@ class TerminalOutputTestCase(unittest.TestCase):
 
     term_output = StringIO()
 
-    def setUp(self):
-        """Redirect terminal output to a StringIO object."""
-        self.term_output = StringIO()
-        sys.stdout = self.term_output
-
-    def tearDown(self):
-        self.term_output.close()
-
     def callScript(self, *cli_args):
-        """Pass the given input parameters to Habitica and store the output."""
+        """
+        Pass the given input parameters to Habitica and store the output.
+
+        Args:
+            *cli_args (:obj:`list` of :obj:`str`): Variable length argument
+                list of the flags you want to pass to the habitica script, e.g.
+                    self.callScript('todos', 'add', '--text="foo"')
+        """
+        term_output = self._redirectStdout()
+
         sys.argv = cli_args
         habitica.cli()
-        sys.stdout = sys.__stdout__  # Without this, assertions fail
-        return self.term_output.getvalue()
+
+        self._resetStdout()
+
+        return self._getStringAndClose(term_output)
+
+    def callScriptTesting(self, *cli_args):
+        """Safer version of callScript that always sends a '--test' flag."""
+        if '-t' not in cli_args and '--test' not in cli_args:
+            cli_args.append('-t')
+        return self.callScript(*cli_args)
 
     def callFunction(self, function, *func_args):
-        """Call function with the given arguments and store the output."""
+        """
+        Call function with the given arguments and store the output.
+
+        Args:
+            *func_args (:obj:`list` of :obj:`str`): Variable length argument
+                list of the arguments you want to pass to the given function.
+        """
+        term_output = self._redirectStdout()
         function(*func_args)
-        sys.stdout = sys.__stdout__  # Without this, assertions fail
-        return self.term_output.getvalue()
+        self._resetStdout()
+
+        return self._getStringAndClose(term_output)
+
+    def _redirectStdout(self):
+        """Redirect terminal output to a StringIO object."""
+        term_output = StringIO()
+        sys.stdout = term_output
+        return term_output
+
+    def _resetStdout(self):
+        """Without doing this, assertions that compare strings fail?."""
+        sys.stdout = sys.__stdout__
+
+    def _getStringAndClose(self, io):
+        """Yank and return the contents of the StringIO object and close it."""
+        output = io.getvalue()
+        io.close()
+        return output
