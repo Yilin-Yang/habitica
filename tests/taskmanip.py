96dd502c9d274c8985d5092727167815b8ab6213
Yilin Yang <yiliny@umich.edu>
2017-12-21 19:28:43 -0500

    Start writing api-dependent test cases
    Start writing taskmanip test case file. Prompt the user to wipe the
    entire test_auth account and don't proceed with tests if the user exits.

---
+++ tests/taskmanip.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Test cases for functions that perform API calls to Habitica.
+"""
+
+import os
+import sys
+import unittest
+
+import helpers
+
+# Make sure we're using dev files, not the installed package
+sys.path.insert(0, os.path.abspath('..'))
+import habitica
+import habitica.taskmanip
+
+
+def setUpModule():
+    """Make sure the developer configured a test account, and nuke it."""
+    sys.argv = ['habitica', '-t', 'status']
+    try:
+        habitica.cli()
+    except:
+        print("Could not find test_auth.cfg file! Create one in the same "
+              "directory as auth.cfg.")
+        sys.exit(1)
+
+    sys.argv = ['habitica', '-t', 'reset']
+    try:
+        habitica.cli()  # TODO: exit if the user answers no to this
+    except SystemExit:
+        print("NOT wiping the user account, so tests will not proceed.")
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+    unittest.main()
