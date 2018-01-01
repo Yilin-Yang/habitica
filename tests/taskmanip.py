33f723aef069c673cc7c3321fb1414f3de682ba1
Yilin Yang <yiliny@umich.edu>
2017-12-31 19:43:51 -0500

    Start writing tag test cases

--- tests/taskmanip.py
+++ tests/taskmanip.py
@@ -5,6 +5,7 @@
 Test cases for functions that perform API calls to Habitica.
 """
 
+import sys
 import unittest
 
 import helpers
@@ -13,10 +14,66 @@ import habitica
 import habitica.taskmanip
 
 
+class TestTagManipulation(helpers.TerminalOutputTestCase):
+
+    to_add = ['foo', 'bar', 'moo', 'gar', 'mar']
+
+    def test_tag_manip(self):
+        # Test adding tags
+        for tag in TestTagManipulation.to_add:
+            output = self.callScriptTesting(
+                *TestTagManipulation._add_tag_args(tag))
+
+        print(output)  # TODO: remove debug
+        self.assertTrue(output == ('[#] 1 foo\n'
+                                   '[#] 2 bar\n'
+                                   '[#] 3 moo\n'
+                                   '[#] 4 gar\n'
+                                   '[#] 5 mar\n'
+                                   )
+                        )
+
+        # Test removing tags by name
+        output = self.callScriptTesting(
+            *TestTagManipulation._delete_tag_args('moo'))
+
+        self.assertTrue(output == ('[#] 1 foo\n'
+                                   '[#] 2 bar\n'
+                                   '[#] 3 gar\n'
+                                   '[#] 4 mar\n'
+                                   )
+                        )
+
+        # Test removing tags by index
+        output = self.callScriptTesting(
+            *TestTagManipulation._delete_tag_args('2,4'))
+
+        self.assertTrue(output == ('[#] 1 foo\n'
+                                   '[#] 2 gar\n'
+                                   )
+                        )
+
+        # Test removing multiple tags by name
+        output = self.callScriptTesting(
+            *TestTagManipulation._delete_tag_args('foo,gar'))
+
+        self.assertTrue(output == '')
+
+    @staticmethod
+    def _add_tag_args(tag_name):
+        """Produce command line arguments to add a tag with a given name."""
+        return ('habitica', '-t', 'tags', 'add', '--text="%s"' % tag_name,)
+
+    @staticmethod
+    def _delete_tag_args(ids_or_names):
+        """Produce command line arguments to delete a tag."""
+        return ('habitica', '-t', 'tags', 'delete', ids_or_names)
+
+
 def setUpModule():
     """Make sure the developer configured a test account, and nuke it."""
     sys.argv = ['habitica', '-t', 'status']
-    try:
+    try:  # TODO: causes double-printing. Is there a better way to do this?
         habitica.cli()
     except:
         print("Could not find test_auth.cfg file! Create one in the same "
